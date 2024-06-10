4a5624834d92aed136966e2b94b4fef83a6b4609
Jake Rasmusson <jakerasmusson@gmail.com>
2024-06-10 15:24:44 -0500

    fixes sequelize config

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,22 @@
-const Sequelize = require('sequelize')
-
+const Sequelize = require('sequelize');
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+  ? new Sequelize(process.env.DB_URL, {
+    dialectOptions: {
+      ssl: {
+        require: false,
+        rejectUnauthorized: false
+      }
+    }
+  })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'postgres',
+      }
+    );
 
 module.exports = sequelize;
